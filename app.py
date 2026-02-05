3b3f66398f6a794780fa207bca32cdb98d41bd2f
gabrielaps28 <gabriela.perez.souza@hotmail.com>
2026-02-05 13:18:13 -0300

    Ajuste no app.py e melhorias do menu

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 
-def exibir_nome_do_programa(): 
+restaurantes = ['Pizza', 'Sushi']
+
+def exibir_nome_do_programa():
     print("""
 𝕊𝕒𝕓𝕠𝕣 𝕖𝕩𝕡𝕣𝕖𝕤𝕤
 """)
@@ -12,30 +14,52 @@ def exibir_opcoes():
     print('4. Sair\n')
 
 def finalizar_app():
-    os.system('cls')  # limpa a tela no Windows
-    # os.system('clear')  # descomente se estiver no Linux/Mac
-    print('Finalizando o app')
+    exibir_subtitulo ('Finalizar app')
+
+def voltar_ao_menu_principal():
+    input('\nDigite uma tecla para voltar ao menu ')
+    main()
 
 def opcao_invalida():
     print('Opção inválida\n')
-    input('Digite uma tecla para voltar ao menu principal')  # Corrigido: 'imput' → 'input'
-    main()
+    voltar_ao_menu_principal()
+
+def exibir_subtitulo(texto):
+    os.system('cls')
+    print(texto)
+    print()
+
+def cadastrar_novo_restaurante():
+    exibir_subtitulo ('Cadastro de novos restaurantes')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+    voltar_ao_menu_principal()
+
+def listar_restaurantes():
+    exibir_subtitulo ('Listando restaurante')
+
+    for restaurante in restaurantes:
+        print(f'- {restaurante}')
+
+    voltar_ao_menu_principal()
 
 def escolher_opcao():
     try:
         opcao_escolhida = int(input('Escolha uma opção: '))
 
-        if opcao_escolhida == 1: 
-            print('Cadastrar restaurante')
-        elif opcao_escolhida == 2: 
-            print('Listar restaurantes')
-        elif opcao_escolhida == 3: 
+        if opcao_escolhida == 1:
+            cadastrar_novo_restaurante()
+        elif opcao_escolhida == 2:
+            listar_restaurantes()
+        elif opcao_escolhida == 3:
             print('Ativar restaurante')
-        elif opcao_escolhida == 4: 
+            voltar_ao_menu_principal()
+        elif opcao_escolhida == 4:
             finalizar_app()
-        else: 
-            opcao_invalida()  # Corrigido: estava 'finalizar_invalidade()' → 'opcao_invalida()'
-    except:
+        else:
+            opcao_invalida()
+    except ValueError:
         opcao_invalida()
 
 def main():
